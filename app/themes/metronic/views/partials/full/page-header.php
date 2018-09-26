b9fc0afa46d50919147a38d2926577c1599cb599
Putra Budiman <cristminix@gmail.com>
2018-09-26 14:57:54 +0700

    up@2018-09-26 14:57:54

--- app/themes/metronic/views/partials/full/page-header.php
+++ app/themes/metronic/views/partials/full/page-header.php
@@ -3,7 +3,7 @@
                 <div class="page-header-inner ">
                     <!-- BEGIN LOGO -->
                     <div class="page-logo">
-                        <a href="index.html">
+                        <a href="{{ base_url }}">
                             <img src="{{ theme_assets }}/layouts/layout/img/logo.png" alt="logo" class="logo-default" /> </a>
                         <div class="menu-toggler sidebar-toggler">
                             <span></span>
--- bin/scripts/remote_script.sh
+++ bin/scripts/remote_script.sh
@@ -6,9 +6,19 @@ rm -v app/logs/*.log
 echo "DELETING CACHE FILES"
 
 rm -f app/cache/*
+
+echo "MKDIR app/cache/{assets,cooke,query}"
+
+mkdir app/cache/assets
+mkdir app/cache/cookie
+mkdir app/cache/query
+
 rm -f app/cache/assets/*
 rm -f app/cache/cookie/*
 rm -f app/cache/query/*
+
+chmod -R 0777 app/cache
+chmod -R 0777 app/cache/*
 
 bash bin/pull
 bash bin/seed
