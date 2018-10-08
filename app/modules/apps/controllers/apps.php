ba0b3cea644897fb54eb53713902bc5109e40a1e
Putra Budiman <cristminix@gmail.com>
2018-10-09 04:08:49 +0700

    -

--- app/modules/apps/controllers/apps.php
+++ app/modules/apps/controllers/apps.php
@@ -1,25 +1,21 @@
-<?php 
-class Apps extends CMS_Controller{
-	function __construct()
-	{
-		# code...
-		parent::__construct();
+<?php
+class Apps extends CMS_Controller
+{
+    function __construct()
+    {
+        # code...
+        parent::__construct();
        
         $this->load->driver('session');
-		// $this->load->model('admin_m');
-		// $this->load->model('download_m');
-		// $this->load->model('galeri_new_m');
-		// $this->load->model('bkpp_m');
-		$this->theme = $this->cms_get_config('site_theme');
-		$this->template->set_theme($this->theme);
-
-	}
-	public function index()
-	{
-		redirect('cluster/grid');
-		$data = [];
-		$config = [];
-
-		$this->view('index',$data,'apps',$config);	
-	}
+        // $this->load->model('admin_m');
+        // $this->load->model('download_m');
+        // $this->load->model('galeri_new_m');
+        // $this->load->model('bkpp_m');
+        $this->theme = $this->cms_get_config('site_theme');
+        $this->template->set_theme($this->theme);
+    }
+    public function index()
+    {
+        redirect('member/entry');
+    }
 }
